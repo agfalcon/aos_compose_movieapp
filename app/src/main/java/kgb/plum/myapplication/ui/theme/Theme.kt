8f4b599e31021cf1b151174d257517440bb99e00
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-05-15 13:01:46 +0900

    [2023.05.15] (theme) Color theme 설정 완료

--- app/src/main/java/kgb/plum/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/kgb/plum/myapplication/ui/theme/Theme.kt
@@ -9,38 +9,22 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import kgb.plum.myapplication.ui.theme.color.ColorSet
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val LocalColors = staticCompositionLocalOf { ColorSet.Red.LightColors.material }
 
 @Composable
 fun MovieAppTheme(
+    myColors: ColorSet,
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -50,8 +34,8 @@ fun MovieAppTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> myColors.DarkColors.material
+        else -> myColors.LightColors.material
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
@@ -62,9 +46,12 @@ fun MovieAppTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+
+    CompositionLocalProvider(LocalColors provides colorScheme) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
